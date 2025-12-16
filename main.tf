b763ba65d23024029a83436fe9d40ae8672d82cd
Petros Zolotas <petros.zolotas@pfizer.com>
2025-12-16 14:50:58 +0200

    remove sg resource

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.1"
-  name = "blog_new"
+  name = "blog"
 
   vpc_id = data.aws_vpc.default.id
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
@@ -40,41 +40,4 @@ module "blog_sg" {
 
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group" "blog" {
-  name = "blog"
-  description = "Allow inbound HTTP & HTTPS traffic. Allow all outbound traffic."
-
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "allow_http_in" {
-  type            = "ingress"
-  from_port       = 80
-  to_port         = 80
-  protocol        = "tcp"
-  cidr_blocks     = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "allow_https_in" {
-  type            = "ingress"
-  from_port       = 443
-  to_port         = 443
-  protocol        = "tcp"
-  cidr_blocks     = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "allow_egress_all" {
-  type = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
 }
